a3c8c0f707b7ba843a7c7f63a7435d08a6ec61c4
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-08-15 21:55:22 -0500

    starting stretch

--- user-onboarding-app/src/App.js
+++ user-onboarding-app/src/App.js
@@ -6,6 +6,9 @@ import OnboardingForm from "./components/Form";
 function App() {
   return (
     <div className="App">
+      <div className="identity">
+        Colby's Totally Not Stealing Your Identity Form
+      </div>
       <OnboardingForm />
     </div>
   );
