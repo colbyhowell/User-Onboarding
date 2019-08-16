9c98a9353df45cf988ed2dd2b1b263f33b585f43
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-08-15 21:44:25 -0500

    Forgot to commit a lot. I'm sorry

--- user-onboarding-app/src/components/Form.js
+++ user-onboarding-app/src/components/Form.js
@@ -1,17 +1,86 @@
 import React, { useEffect, useState } from "react";
 import { Form, Field, withFormik } from "formik";
 import * as Yup from "yup";
-import axious from "axios";
+import axios from "axios";
 
-const OnboardingForm = () => {
+const OnboardingForm = ({ errors, touched, values, status }) => {
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    if (status) {
+      setUserList([...userList, status]);
+    }
+  }, [status]);
+
   return (
-    <div className="onboarding-form-container">
-      <Form>
+    <div className="">
+      <Form className="onboarding-form-container">
         <Field type="text" name="name" placeholder="Enter Name" />
+        {touched.name && errors.name && (
+          <p className="error-message">{errors.name}</p>
+        )}
         <Field type="text" name="email" placeholder="Enter Email" />
+        {touched.email && errors.email && (
+          <p className="error-message">{errors.email}</p>
+        )}
+        <Field type="password" name="password" placeholder="Enter Password" />
+        {touched.password && errors.password && (
+          <p className="error-message">{errors.password}</p>
+        )}
+        <label className="terms-checkbox">
+          Accept Terms Of Service?
+          <Field type="checkbox" name="terms" checked={values.terms} />
+          {touched.terms && errors.terms && (
+            <p className="error-message">{errors.terms}</p>
+          )}
+        </label>
+        <button type="submit">Submit!</button>
       </Form>
+      {userList.map(user => (
+        <p key={user.id}>{user.name}</p>
+      ))}
     </div>
   );
 };
 
-export default OnboardingForm;
+const formikPassValues = withFormik({
+  mapPropsToValues({ name, email, password, terms }) {
+    return {
+      name: name || "",
+      email: email || "",
+      password: password || "",
+      terms: terms || false
+    };
+  },
+  validationSchema: Yup.object().shape({
+    name: Yup.string()
+      .min(1, "Come on you know your name is longer than that")
+      .max(20, "That is a long ass name")
+      .required("please enter your name"),
+    email: Yup.string()
+      .email()
+      .required("must be an email"),
+    password: Yup.string()
+      .min(6, "Do better")
+      .max(20, "Woah slow down")
+      .required(),
+    terms: Yup.boolean().oneOf([true], "You must accept the terms of service")
+  }),
+
+  handleSubmit(values, { setStatus, resetForm }) {
+    axios
+      .post("https://reqres.in/api/users", values)
+      .then(res => {
+        console.log("Response here!", res);
+        setStatus(res.data);
+        resetForm();
+      })
+      .catch(err => {
+        console.log("error", err);
+      });
+  }
+});
+
+const formWithUserData = formikPassValues(OnboardingForm);
+
+export default formWithUserData;
